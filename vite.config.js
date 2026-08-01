583d4e6bfae779ef00b280322b60bc176e334dcc
Mikhail Pisemsky <mikhailpisemsky@yandex.ru>
2026-08-01 12:27:55 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/ML_image_upgrader/',
   server: {
     port: 3000,
     open: true,
   },
-  publicDir: 'public',
 });
